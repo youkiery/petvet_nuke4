64a649cec9141bbded4a50e959bd6fd74d45b418
youkiery <youkiery@gmail.com>
2019-01-28 07:34:44 +0700

    sửa lại bộ lọc tiêm phòng

--- modules/vaccine/global.functions.php
+++ modules/vaccine/global.functions.php
@@ -1231,7 +1231,7 @@ function user_vaccine() {
       $where = "where calltime between $from and $end";
   }
   
-  $sql = "select a.id, a.note, a.recall, b.id as petid, b.name as petname, c.id as customerid, c.name as customer, c.phone as phone, cometime, calltime, ctime, status, diseaseid, dd.name as disease from " . VAC_PREFIX . "_vaccine a inner join " . VAC_PREFIX . "_pet b on a.petid = b.id inner join " . VAC_PREFIX . "_customer c on b.customerid = c.id inner join " . VAC_PREFIX . "_disease dd on a.diseaseid = dd.id $where order by calltime";
+  $sql = "select a.id, a.note, a.recall, b.id as petid, b.name as petname, c.id as customerid, c.name as customer, c.phone as phone, cometime, calltime, ctime, status, diseaseid, dd.name as disease from " . VAC_PREFIX . "_vaccine a inner join " . VAC_PREFIX . "_pet b on a.petid = b.id inner join " . VAC_PREFIX . "_customer c on b.customerid = c.id inner join " . VAC_PREFIX . "_disease dd on a.diseaseid = dd.id $where and status = $id order by calltime";
   $query = $db->query($sql);
   $list = fetchall($db, $query);
   return vaccine_list($list);
