1719a52a13f1f826c680076cf7bfa236d7dfaeea
youkiery <youkiery@gmail.com>
2019-06-11 10:37:46 +0700

    biograph: nothing

--- modules/biograph/admin/main.php
+++ modules/biograph/admin/main.php
@@ -11,86 +11,19 @@ if (!defined('NV_IS_ADMIN_FORM')) {
 	die('Stop!!!');
 }
 
-define('READ_ONLY', 1);
-define('MODIFY', 2);
-
-$page_title = "Nhập hồ sơ một chiều";
-
-$action = $nv_Request->get_string('action', 'post/get', "");
-if (!empty($action)) {
-	$result = array("status" => 0);
-	switch ($action) {
-		case 'search':
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			$result['status'] = 1;
-			$result['html'] = employerList($key);
-		break;
-		case 'filterEmploy':
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			$result['status'] = 1;
-			$result['html'] = notAllowList($key);
-		break;
-		case 'addEmploy':
-			$userid = $nv_Request->get_string('userid', 'get/post', '');
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			if (checkEmptyPemission($userid)) {
-				$sql = 'insert into `'. $db_config['prefix'] .'_user_allow` (userid, module, type) values('.$userid.', '. PERMISSION_MODULE .', 1)';
-				if ($db->query($sql)) {
-					$result['status'] = 1;
-					$result['html'] = employerList($key);
-					$result['notify'] = 'Đã thêm nhân viên mới';
-				}
-			}
-		break;
-		case 'removeUser':
-			$userid = $nv_Request->get_string('userid', 'get/post', '');
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			if ($id = checkUserPermission($userid)) {
-				$sql = 'delete from `'. $db_config['prefix'] .'_user_allow` where id = ' . $id;
-				if ($db->query($sql)) {
-					$result['status'] = 1;
-					$result['html'] = employerList($key);
-					$result['notify'] = 'Đã bỏ quyền thành viên';
-				}
-			}
-		break;
-		case 'upUser':
-			$userid = $nv_Request->get_string('userid', 'get/post', '');
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			if ($id = checkUserPermission($userid)) {
-				$sql = 'update `'.$db_config['prefix'].'_user_allow` set type = 2 where id = ' . $id;
-				if ($db->query($sql)) {
-					$result['status'] = 1;
-					$result['html'] = employerList($key);
-				}
-			}
-		break;
-		case 'downUser':
-			$userid = $nv_Request->get_string('userid', 'get/post', '');
-			$key = $nv_Request->get_string('key', 'get/post', '');
-
-			if ($id = checkUserPermission($userid)) {
-				$sql = 'update `'.$db_config['prefix'].'_user_allow` set type = 1 where id = ' . $id;
-				if ($db->query($sql)) {
-					$result['status'] = 1;
-					$result['html'] = employerList($key);
-				}
-			}
-		break;
-	}
-
-	echo json_encode($result);
-	die();
-}
+$page_title = "Quản lý chíp";
 
 $xtpl = new XTemplate("main.tpl", PATH);
 
-$xtpl->assign('content', employerList());
+$step = $nv_Request->get_int('s', 'get', 1);
+
+if ($step <= 1) {
+	$customer = $nv_Request->get_string('customer', 'get', '');
+	$xtpl->assign('customer', $customer);
+	$xtpl->parse('main.customer');
+}
+
+$xtpl->assign('customer', $customer);
 $xtpl->parse("main");
 $contents = $xtpl->text("main");
 
