16fbe0c359dbf3f0c5574c0aea5e7cd41056c313
youkiery <youkiery@gmail.com>
2019-06-24 15:48:19 +0700

    bio: file

---
+++ modules/biograph/layout/header.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="/modules/biograph/src/bootstrap.min.css">
+<link rel="stylesheet" href="/modules/biograph/src/style.css">
+<script src="/modules/biograph/src/jquery.min.js"></script>
+<script src="/modules/biograph/src/bootstrap.min.js"></script>
