7433661fad9475fca67cabae1532cbb8f8a1664c
youkiery <youkiery@gmail.com>
2019-10-13 10:02:31 +0700

    init

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+  // $db = new mysqli('localhost', 'root', '', 'petcoffee2');
+  // if ($db->connect_error) die('DB error');
+
+  // $query = $db->query('select * from pet_news_user');
+  // if ($row = $query->fetch_assoc()) {
+  //   var_dump($row);
+  // }
+  // else die($db->error());
+?>
