df15d5724a6e7575e1bdafbea890b9ea219fd401
youkiery <youkiery@gmail.com>
2019-10-28 15:03:35 +0700

    khởi tạo module excel

---
+++ modules/excel/version.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author Frogsis
+ * @Createdate Tue, 28 Oct 2019 15:00:00 GMT
+ */
+
+if (!defined('NV_MAINFILE'))
+    die('Stop!!!');
+
+$module_version = array(
+    'name' => 'Excel',
+    'modfuncs' => 'main',
+    'submenu' => 'main',
+    'is_sysmod' => 0,
+    'virtual' => 1,
+    'version' => '4.3.01',
+    'date' => 'Tue, 14 Nov 2017 01:50:53 GMT',
+    'author' => 'Frogsis',
+    'uploads_dir' => array($module_name),
+    'note' => ''
+);
