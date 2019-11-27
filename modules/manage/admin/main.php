e5de04c8323ea6aea65fb43f210dad060931088b
youkiery <youkiery@gmail.com>
2019-11-27 16:56:38 +0700

    comment

--- modules/manage/admin/main.php
+++ modules/manage/admin/main.php
@@ -12,8 +12,16 @@ $action = $nv_Request->get_string('action', 'post', '');
 if (!empty($action)) {
   $result = array('status' => 0);
   switch ($action) {
-    case 'done-check':
-      $x = 1;
+    case 'excel':
+    $data = $nv_Request->get_array('data', 'post');
+
+    foreach ($data as $index => $row) {
+      foreach ($row as $name => $value) {
+        if (!($name == '6' || $name == '3' || $name == '7')) checkRemind($name, $value);
+      }
+      $sql = 'insert into `'. PREFIX .'device` (name, unit, number, year, intro, status, depart, source, description, import_time, update_time) values("'. $row[0] .'", "'. $row[2] .'", "'. $row[3] .'", "'. $row[4] .'", "'. $row[1] .'", "", \'["'. $row[7] .'"]\', "'. $row[5] .'", "'. $row[6] .'", '. totime($row['import']) .', '. time() .')';
+      $db->query($sql);
+    }
     break;
   }
   echo json_encode($result);
