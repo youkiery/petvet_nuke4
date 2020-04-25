5f91daa16419a00ff2bd86723dc51e75274a1dba
youkiery@gmail.com <youkiery@gmail.com>
2020-04-25 14:50:36 +0700

    thêm module gen

---
+++ modules/gen/admin.menu.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @Project Petcoffee-tech
+ * @Chistua (hchieuthua@gmail.com)
+ * @Copyright (C) 2019
+ * @Createdate 21-03-2019 13:15
+ */
+
+if (!defined('NV_ADMIN')) {
+    die('Stop!!!');
+}
+
+// $submenu['center'] = "Quản lý yêu cầu trang trại";
+
+$allow_func = array('main'); 
---
+++ modules/gen/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Project Petcoffee-tech
+ * @Chistua (hchieuthua@gmail.com)
+ * @Copyright (C) 2019
+ * @Createdate 21-03-2019 13:15
+ */
+
+if (!defined('NV_SYSTEM')) {
+  die('Stop!!!');
+}
+
+define('NV_IS_FORM', true); 
+define("PATH", NV_ROOTDIR . '/modules/' . $module_file . '/template/user/' . $op);
