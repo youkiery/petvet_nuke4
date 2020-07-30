46b886f31e1da8e351681a7250343bc1fd5bb297
youkiery <youkiery@gmail.com>
2020-07-30 15:29:05 +0700

    remove form

--- modules/form/language/vi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$lang_translator['author'] = "Chistua (hchieuthua@gmail.com)";
-$lang_translator['createdate'] = "21/03/2019, 13:15";
-$lang_translator['copyright'] = "@Copyright (C) 2019";
-$lang_translator['info'] = "";
-$lang_translator['langtype'] = "lang_module";
-
