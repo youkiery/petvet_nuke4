518cc81495f846c07ec79a7a8069862ec85244d2
youkiery <youkiery@gmail.com>
2021-03-22 09:47:53 +0700

    init tester

---
+++ modules/tester/funcs/main.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @Project Mining 0.1
+ * @Author Frogsis
+ * @Createdate Mon, 28 Oct 2019 15:00:00 GMT
+ */
+
+if (!defined('NV_IS_MOD_CONGVAN')) die('Stop!!!');
+
+include NV_ROOTDIR . '/includes/header.php';
+echo nv_site_theme($contents);
+include NV_ROOTDIR . '/includes/footer.php';
